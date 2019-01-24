9364476675cf1ae8a635794430dcb720045cac37
root <alexander.steiger@fischer.de>
2019-01-24 15:04:09 +0100

    integration WEB server 1.0.0 with some modifications; see #15

--- board/FT/TXT/rootfs/var/www/wifiadmin/include/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Some Constants */
-error_reporting(E_ALL); // Betray all worms...
-
-/* Program Version : Used to populate install.php changes and for display reasons */
-$I_VERSION = "0.2.6b1";
-
-?>
