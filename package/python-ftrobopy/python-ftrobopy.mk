080866c2dcba7ece0ea60cdc92c641d41b5e944e
Alexander Steiger <alexander.steiger@fischer.de>
2020-03-26 08:18:31 +0100

    changed make file ftrobopy (master)

--- package/python-ftrobopy/python-ftrobopy.mk
+++ package/python-ftrobopy/python-ftrobopy.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_FTROBOPY_VERSION = d29215c744f572d1d23a536d8ab17191cdd65810
-PYTHON_FTROBOPY_SITE = $(call github,ftrobopy,ftrobopy,$(PYTHON_FTROBOPY_VERSION))
+PYTHON_FTROBOPY_VERSION = master
+PYTHON_FTROBOPY_SITE_METHOD = git
+PYTHON_FTROBOPY_SITE = https://github.com/ftrobopy/ftrobopy.git
 PYTHON_FTROBOPY_LICENSE = MIT
 PYTHON_FTROBOPY_LICENSE_FILES = LICENSE
 PYTHON_FTROBOPY_INSTALL_STAGING = NO
-PYTHON_FTROBOPY_SETUP_TYPE = distutils
 PYTHON_FTROBOPY_DEPENDENCIES = python-serial
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
@@ -18,5 +18,12 @@ else ifeq ($(BR2_PACKAGE_PYTHON3),y)
 PYTHON_FTROBOPY_DEPENDENCIES += python3
 endif
 
-$(eval $(python-package))
+define PYTHON_FTROBOPY_INSTALL_PY_STUFF
+	$(INSTALL) -D -m 0644 $(@D)/ftrobopy.py $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/ftrobopy/ftrobopy.py
+	$(INSTALL) -D -m 0644 $(@D)/LICENSE.txt $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/ftrobopy/LICENSE.txt
+	$(INSTALL) -D -m 0644 $(@D)/__init__.py $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/ftrobopy/__init__.py
+endef
+PYTHON_FTROBOPY_POST_INSTALL_TARGET_HOOKS += PYTHON_FTROBOPY_INSTALL_PY_STUFF
+
+$(eval $(generic-package))
 
